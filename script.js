a314df6595deaaaa420f1c9ed1f088e8a0f9a16b
Connor <174837826+royalduck6@users.noreply.github.com>
2025-05-19 20:04:57 -0700

    Changed winstreak feature to a score feature. Made the game end after 5 rounds. Added more QOL.

--- script.js
+++ script.js
@@ -2,63 +2,58 @@ function playGame(computerMove, playerMove) {
     if (playerMove == "rock") {
         switch (computerMove) {
             case "rock":
-                console.log("The computer played rock");
+                console.log("You played rock and the computer played rock");
                 console.log("Tie");
                 break;
             case "paper":
-                console.log("The computer played paper");
+                console.log("You played rock and the computer played paper");
                 console.log("Computer wins");
-                setWinstreak("computer")
+                setScore("computer")
                 break;
             case "scissors":
-                console.log("The computer played scissors");
+                console.log("You played rock and the computer played scissors");
                 console.log("Player wins!");
-                setWinstreak("player")
+                setScore("player")
                 break;
         }
     } else if (playerMove == 'paper') {
         switch (computerMove) {
             case "rock":
-                console.log("The computer played rock");
+                console.log("You played paper and the computer played rock");
                 console.log("Player Wins!")
-                setWinstreak("player")
+                setScore("player")
                 break;
             case "paper":
-                console.log("The computer played paper");
+                console.log("You played paper and the computer played paper");
                 console.log("Tie")
                 break;
             case "scissors":
-                console.log("The computer played scissors");
+                console.log("You played paper and the computer played scissors");
                 console.log("Computer wins");
-                setWinstreak("computer")
+                setScore("computer")
                 break;
     }
     } else {
         switch (computerMove) {
             case "rock":
-                console.log("The computer played rock");
+                console.log("You played scissors and the computer played rock");
                 console.log("Computer wins");
-                setWinstreak("computer");
+                setScore("computer");
                 break;
             case "paper":
-                console.log("The computer played paper");
+                console.log("You played scissors and the computer played paper");
                 console.log("Player wins!");
-                setWinstreak("player");
+                setScore("player");
                 break;
             case "scissors":
-                console.log("The computer played scissors");
+                console.log("You played scissors and the computer played scissors");
                 console.log("Tie")
                 break;
                 }
     }
 }
-function setWinstreak(winner) {
-    if (winstreak[1] == winner) {
-                winstreak[0]++;
-                } else {
-                    winstreak[0] = 1;
-                    winstreak[1] = winner
-                }
+function setScore(winner) {
+    score[winner]++;
 }
 function setMoves() {
     let playerMove = prompt("Choose a move (rock/paper/scissors)");
@@ -68,14 +63,18 @@ function setMoves() {
 function gameLoop() {
     let [playerMove, computerMove] = setMoves();
     playGame(playerMove, computerMove);
-    console.log(`The ${winstreak[1]} has a winstreak of ${winstreak[0]}`);
-    again = prompt("Play again (y/n)");
-    if (again == "y") {
+    console.log(`The score is Player : ${score["player"]} to Computer : ${score["computer"]}`);
+    gamesPlayed++;
+    if (gamesPlayed !== 5) {
         gameLoop();
     }
 }
 
-let winstreak = [0, "player"]
+let gamesPlayed = 0;
+let score = {
+    "player": 0,
+    "computer": 0,
+}
 const computerMoves = ["rock", "paper", "scissors"];
 
 gameLoop();
